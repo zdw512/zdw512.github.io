f1a801940048b55334d1bd2ba11adeed3bbaa83a
zwd512 <1363468974@qq.com>
2024-10-24 18:27:21 +0800

    Site updated: 2024-10-24 18:27:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/23/hello-world/","2024/10/24/待送达/","2024/10/24/Untitled/","2024/10/24/在俩一次/","2024/10/24/洒水/"];function toRandomPost(){
+var posts=["2024/10/24/Untitled/","2024/10/24/在俩一次/","2024/10/23/hello-world/","2024/10/24/待送达/","2024/10/24/洒水/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
